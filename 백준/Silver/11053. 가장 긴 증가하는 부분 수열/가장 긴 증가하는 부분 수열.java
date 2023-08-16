ea7db71c7296079ccadeda7d24f682fee713362b
rightpair <tkddn204@gmail.com>
2023-08-16 12:54:01 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 160 ms, Memory: 14740 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    static int[] dp;
+    private static void solve(BufferedReader br) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] inp = Arrays.stream(br.readLine().split(" "))
+            .mapToInt(Integer::parseInt).toArray();
+        dp = new int[n];
+        dp[0] = 1;
+        int maxCnt = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (inp[j] < inp[i] && dp[i] < dp[j] + 1) {
+                    dp[i]++;
+                    maxCnt = Math.max(maxCnt, dp[i]);
+                }
+            }
+        }
+        res.append(maxCnt);
+    }
+
+    private static StringBuilder res;
+    public static void main(String args[]) throws Exception {
+        if (args.length > 1) fileInputOutput(args[0], args[1]);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            res = new StringBuilder();
+            solve(br);
+            bw.write(res.toString());
+        }
+    }
+
+    private static void fileInputOutput(String inputPath, String outputPath) throws IOException {
+        System.setIn(new FileInputStream(inputPath));
+        System.setOut(
+            new PrintStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(outputPath)
+                )));
+    }
+}
