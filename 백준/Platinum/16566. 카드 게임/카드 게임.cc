63ee21602eab17ac663e43c4da189eb6a577a1d2
rightpair <tkddn204@gmail.com>
2023-09-02 10:48:04 +0900

    [Platinum V] Title: 카드 게임, Time: 728 ms, Memory: 33276 KB -BaekjoonHub

--- 백준/Platinum/16566. 카드 게임/카드 게임.cc
+++ 백준/Platinum/16566. 카드 게임/카드 게임.cc
@@ -4,34 +4,49 @@ using namespace std;
 #define endl "\n"
 
 int card[4000001];
-bool chk[4000001];
+int parent[4000001];
+
+int getParent(int x) {
+  if (x == parent[x]) return parent[x];
+  return parent[x] = getParent(parent[x]);
+}
+
+void uniParent(int x, int y) {
+  x = getParent(x);
+  y = getParent(y);
+  
+  if (x < y) parent[x] = y;
+  if (x > y) parent[y] = x;
+}
 
 void solve() {
   int n, m, k;
   cin >> n >> m >> k;
 
   for (int i = 0; i < m; i++) {
-    cin >> card[i];    
+    cin >> card[i];
+    parent[i] = i;
   }
   sort(card, card + m);
 
   while (k--) {
     int num;
     cin >> num;
-
+    
     int st = 0;
     int en = m - 1;
     while (st < en) {
       int mid = (st + en) / 2;
-      if (card[mid] < num) {
+      if (card[mid] <= num) {
         st = mid + 1;
       } else {
         en = mid;
       }
     }
-    while (chk[en] || num >= card[en]) en++;
-    chk[en] = true;
+
+    en = getParent(en);
     cout << card[en] << endl;
+    uniParent(en, en + 1);
   }
 }
 
