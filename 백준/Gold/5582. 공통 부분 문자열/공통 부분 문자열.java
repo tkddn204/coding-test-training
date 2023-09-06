efcdaaa840220ec2cf4a938f9a186e69548cbad7
rightpair <tkddn204@gmail.com>
2023-09-06 14:25:50 +0900

    [Gold V] Title: 공통 부분 문자열, Time: 252 ms, Memory: 77120 KB -BaekjoonHub

---
+++ 백준/Gold/5582. 공통 부분 문자열/공통 부분 문자열.java
@@ -0,0 +1,45 @@
+import java.io.*;
+
+class Main {
+
+    static int[][] dp;
+
+    private static void solve(BufferedReader br) throws IOException {
+        String s = br.readLine();
+        String t = br.readLine();
+        int n = s.length();
+        int m = t.length();
+        dp = new int[n + 1][m + 1];
+
+        int max = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                    max = Math.max(max, dp[i][j]);
+                }
+            }
+        }
+        res.append(max);
+    }
+
+    private static StringBuilder res;
+    public static void main(String args[]) throws Exception {
+        if (args.length > 1) fileInputOutput(args[0], args[1]);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            res = new StringBuilder();
+            solve(br);
+            bw.write(res.toString());
+        }
+    }
+
+    private static void fileInputOutput(String inputPath, String outputPath) throws IOException {
+        System.setIn(new FileInputStream(inputPath));
+        System.setOut(
+            new PrintStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(outputPath)
+                )));
+    }
+}
