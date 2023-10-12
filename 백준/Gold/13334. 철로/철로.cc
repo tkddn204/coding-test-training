fa1e9bc093ca34caffa2f90d1c4cf00af583bcd4
rightpair <tkddn204@gmail.com>
2023-10-12 12:41:27 +0900

    [Gold II] Title: 철로, Time: 40 ms, Memory: 5224 KB -BaekjoonHub

---
+++ 백준/Gold/13334. 철로/철로.cc
@@ -0,0 +1,65 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define endl "\n"
+
+vector<pair<int, int>> arr;
+priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+
+void solve() {
+  int n;
+  cin >> n;
+  for (int i = 0; i < n; i++) {
+    int s, t;
+    cin >> s >> t;
+    s += 100000000;
+    t += 100000000;
+    if (s > t) swap(s, t);
+    arr.push_back({s, t});
+  }
+  int d;
+  cin >> d;
+
+  auto cmp = [&](pair<int,int> a, pair<int,int> b) -> bool {
+    auto [ast, aen] = a;
+    auto [bst, ben] = b;
+    
+    if (aen == ben) return ast < bst;
+    return aen < ben;
+  };
+
+  sort(arr.begin(), arr.end(), cmp);
+  
+  int mx = 0;
+  int st = 0, en = d;
+  for (auto [l, r] : arr) {
+    if (r - l > d) continue;
+    pq.push({l, r});
+    
+    // 범위 조정
+    if (!(st <= l && r <= en)) {
+      en = r;
+      st = en - d;
+    }
+
+    // 뺄거 빼기
+    while (!pq.empty() && !(st <= pq.top().first && pq.top().second <= en)) {
+      pq.pop();
+    }
+
+    mx = max(mx, (int)pq.size());
+  }
+  cout << mx;
+}
+
+int main() {
+#ifndef ONLINE_JUDGE
+  freopen("input.txt", "r", stdin);
+  freopen("output.txt", "w", stdout);
+#else
+  ios_base::sync_with_stdio(false);
+  cin.tie(0);
+#endif
+  solve();
+  return 0;
+}
