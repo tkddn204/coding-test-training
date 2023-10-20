a9a65a470b1f89c8c9540fc75ed73b0ddcb2f92d
rightpair <tkddn204@gmail.com>
2023-10-20 12:59:19 +0900

    [level 3] Title: 양과 늑대, Time: 1.16 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/92343. 양과 늑대/양과 늑대.java
@@ -0,0 +1,47 @@
+import java.util.*;
+
+class Solution {    
+    int n, answer;
+    int[] info;
+    boolean[][][] vst;
+    ArrayList<ArrayList<Integer>> tree;
+    
+    void dfs(int cur, int sheep, int wolf) {
+        if (info[cur] == 0) sheep++;
+        else if (info[cur] == 1) wolf++;
+        if (sheep == wolf) return;
+        
+        answer = Math.max(answer, sheep);
+        
+        for (int nxt : tree.get(cur)) {
+            if (vst[nxt][sheep][wolf]) continue;
+            
+            info[cur] -= 2;
+            vst[cur][sheep][wolf] = true;
+            
+            dfs(nxt, sheep, wolf);
+            
+            info[cur] += 2;
+            vst[cur][sheep][wolf] = false;
+        }
+    }
+    
+    public int solution(int[] _info, int[][] edges) {
+        n = _info.length;
+        info = _info;
+        vst = new boolean[n + 1][n + 1][n + 1];
+        tree = new ArrayList<>();
+        
+        for (int i = 0; i <= n; i++) {
+            tree.add(new ArrayList<>());
+        }
+        for (int i = 0; i < edges.length; i++) {
+            tree.get(edges[i][0]).add(edges[i][1]);
+            tree.get(edges[i][1]).add(edges[i][0]);
+        }
+        
+        dfs(0, 0, 0);
+        
+        return answer;
+    }
+}
