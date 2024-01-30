e84c196f90528a0031b0c58781b8521b81c3baff
rightpair <tkddn204@gmail.com>
2024-01-30 10:54:35 +0900

    [level 1] Title: 폰켓몬, Time: 7.91 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.java
@@ -10,17 +10,11 @@ class Solution {
             pon.put(num, pon.getOrDefault(num, 0) + 1);
         }
         
-        List<Integer> keys = new ArrayList<>(pon.keySet());
-        keys.sort((a, b) -> pon.get(a).compareTo(pon.get(b)));
-        
         int sum = 0;
-        for (int key : keys) {
+        for (int key : pon.keySet()) {
             sum++;
             if (sum >= n / 2) break;
         }
-        
-        // 포켓몬 종류 번호 갯수
-        
         return sum;
     }
 }
