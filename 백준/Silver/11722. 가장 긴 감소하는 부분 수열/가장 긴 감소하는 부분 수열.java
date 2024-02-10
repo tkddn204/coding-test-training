d24f057462c4829cb33d017935413c36513e65fa
rightpair <tkddn204@gmail.com>
2024-02-10 18:01:53 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 148 ms, Memory: 14772 KB -BaekjoonHub

---
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.*;
+
+class Main {
+
+    private static void solve(BufferedReader br) throws IOException {
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        int ans = 0;
+        int[] dp = new int[n];
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (arr[i] < arr[j]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            ans = Math.max(ans, dp[i]);
+        }
+        res.append(ans);
+    }
+
+    private static StringBuilder res;
+    public static void main(String args[]) throws Exception {
+        if (args.length > 1) fileInputOutput(args[0], args[1]);
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+             BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out))) {
+            res = new StringBuilder();
+            solve(br);
+            bw.write(res.toString());
+        }
+    }
+
+    private static void fileInputOutput(String inputPath, String outputPath) throws IOException {
+        System.setIn(new FileInputStream(inputPath));
+        System.setOut(
+            new PrintStream(
+                new BufferedOutputStream(
+                    new FileOutputStream(outputPath)
+                )));
+    }
+}
