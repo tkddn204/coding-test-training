df7a2ce65473c597ec5db156ed4353cfdd816930
rightpair <tkddn204@gmail.com>
2024-02-26 12:30:16 +0900

    [level 2] Title: 타겟 넘버, Time: 1.73 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,13 @@
 class Solution {
-    int answer = 0;
-    void go(int n, int k, int[] numbers, int target) {
+    int go(int n, int k, int[] numbers, int target) {
         if (k == numbers.length) {
-            if (n == target) answer++;
-            return;
-        }
-        
-        go(n + numbers[k], k + 1, numbers, target);
-        go(n - numbers[k], k + 1, numbers, target);
+            if (n == target) return 1;
+            return 0;
+        }   
+        return go(n + numbers[k], k + 1, numbers, target) + go(n - numbers[k], k + 1, numbers, target);
     }
     
     public int solution(int[] numbers, int target) {
-        go(0, 0, numbers, target);
-        
-        return answer;
+        return go(0, 0, numbers, target);
     }
 }
