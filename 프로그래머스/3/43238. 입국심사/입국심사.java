f40f1919b41d40d864bc689fea362c6f93d919cf
rightpair <tkddn204@gmail.com>
2024-02-28 18:07:54 +0900

    [level 3] Title: 입국심사, Time: 0.11 ms, Memory: 75.4 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,32 @@
+class Solution {
+    
+    int[] times;
+    
+    public boolean go(long n, long mid) {
+        long res = 0;
+        for (int time: times) {
+            res += mid / time;
+            if (res >= n) return false;
+        }
+        return res < n;
+    }
+    
+    public long solution(int n, int[] _times) {
+        long answer = 0;
+        times = _times;
+        int size = _times.length;
+        
+        long st = 0;
+        long en = Long.MAX_VALUE;
+        while (st <= en) {
+            long mid = (st + en) / 2;
+            if (go((long) n, mid)) st = mid + 1;
+            else {
+                en = mid - 1;
+                answer = mid;
+            }
+        }
+        
+        return answer;
+    }
+}
